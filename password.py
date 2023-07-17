4a9ff727ccabf96d0dee51194bde6fb177f92c3d
Soap <573471669@qq.com>
2023-07-17 13:50:58 +0800

    Print password.

--- password.py
+++ password.py
@@ -1,7 +1,4 @@
 MINIMUM_LENGTH = 8
 
 password = input("Please enter your password: ")
-while len(password) < MINIMUM_LENGTH:
-    print("Your password doesn't meet a minimum length set.")
-    password = input("Please enter your password: ")
-print("*" * len(password))
+
